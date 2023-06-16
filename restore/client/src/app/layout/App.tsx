a0d94c5c8bd0e4f3e496e921de5672bc3295b6e3
Romain <romain.gioux@felfel.ch>
2023-06-16 13:56:27 -0600

    adding react router

--- restore/client/src/app/layout/App.tsx
+++ restore/client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
 import { useState } from "react"
 import Header from "./Header"
-import Catalog from "../../features/catalog/Catalog"
+import { Outlet } from "react-router-dom";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +24,7 @@ function App() {
             <CssBaseline />
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Catalog />
+                <Outlet />
             </Container>
         </ThemeProvider>
     );
